bba32705becaab339eae0f6005ee11be9da21ada
tilav <tilav@tilav.tilav>
2023-06-07 23:32:52 +0500

    dev jobs mode btn

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const darkBtn = document.querySelector('.dark-btn')
+const darkModeBtn = document.querySelector('.dark-mode-btn')
+
+
+
+
+
+
+
+
+
+
+
+
+
+darkBtn.addEventListener('click', ()=> {
+    darkModeBtn.classList.toggle('dark-mode-btn-click')
+})
