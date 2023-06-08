74732b763a714164d6b00673c6623dac95bbc523
tilav <tilav@tilav.tilav>
2023-06-08 16:11:22 +0500

    search btn for filter by title

--- js/main.js
+++ js/main.js
@@ -2,14 +2,44 @@ const darkBtn = document.querySelector('.dark-btn')
 const darkModeBtn = document.querySelector('.dark-mode-btn')
 const jobsBox = document.querySelector('.jobs-box')
 const learnMoreBtn = document.querySelector('.learn-more-btn')
+const searchBtn = document.querySelector('.search-btn')
 const API = 'https://db-json-one.vercel.app/db.json'
 
 let maxJob = 12
 
 learnMoreBtn.addEventListener('click', ()=> {
-    maxJob += 18
+    maxJob += 3
     jobsBox.innerHTML = ''
     recFnc()
+})
+
+searchBtn.addEventListener('click', ()=> {
+    jobsBox.innerHTML = ''
+    let filterByTitle = document.querySelector('#title')
+    let filterByLocated = document.querySelector('#location')
+    fetch(API)
+    .then((res)=> res.json())
+    .then((datas)=> {
+        datas.forEach((data, i)=> {
+            if(data.title.includes(filterByTitle.value.trim()) && filterByTitle.value.trim() !== ""){
+                const item = `
+                            <div class="job-box">
+                                <img src="${data.logo}" alt="icon image">
+                                <div class="job-title">
+                                    <p><span>${data.whenPlaced}</span>.<span>${data.workTime}</span></p>
+                                    <h3>${data.title}</h3>
+                                    <p>${data.companyName}</p>
+                                </div>
+                                <span>${data.companyCountry}</span>
+                            </div>
+                            `
+                jobsBox.innerHTML += item
+                console.log(filterByLocated);
+            }else if(filterByTitle.value.trim() === "" && i === 0) {
+                recFnc()
+            }
+        })
+    })
 })
 
 const recFnc = ()=> {
