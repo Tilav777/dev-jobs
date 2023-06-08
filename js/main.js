9d9576de7ddbaa3d6a23b13a774023e124e9703a
tilav <tilav@tilav.tilav>
2023-06-08 11:47:01 +0500

    dev jobs API res create

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 const darkBtn = document.querySelector('.dark-btn')
 const darkModeBtn = document.querySelector('.dark-mode-btn')
+const jobsBox = document.querySelector('.jobs-box')
 
+const API = 'https://db-json-one.vercel.app/db.json'
 
+fetch(API)
+    .then((res)=> res.json())
+    .then((datas)=> {
+        datas.forEach((data,i)=> {
+            if(i < 12){
+                const item = `
+                            <div class="job-box">
+                                <img src="${data.logo}" alt="icon image">
+                                <div class="job-title">
+                                    <p><span>${data.whenPlaced}</span>.<span>${data.workTime}</span></p>
+                                    <h3>${data.title}</h3>
+                                    <p>${data.companyName}</p>
+                                </div>
+                                <span>${data.companyCountry}</span>
+                            </div>
+                            `
+                jobsBox.innerHTML += item
+
+            }
+
+        })
+    })
+    .catch(()=> {
+        console.error('error');
+    })
 
 
 
